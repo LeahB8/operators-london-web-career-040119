897ed4fca8dfbd34a17aa0f38bb920d9707cee8d
Leah Boyd <leahboyd08@gmail.com>
2019-03-16 18:26:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,13 +13,6 @@ end
 
 
 def not_safe?(speed)
-    if speed < 40
-    return true
-  end
-  if speed > 60
-    return true
-  end
 	if speed < 40 && speed > 60 ? "true" : "false"
-	return false
 	end
 end
